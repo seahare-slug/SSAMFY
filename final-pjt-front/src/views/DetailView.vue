6235a99f2f1874a1888e57d4f22cb4ceac074a7b
song <busanbak2000@naver.com>
2022-11-23 12:12:17 +0900

    🔰update DetailView

--- final-pjt-front/src/views/DetailView.vue
+++ final-pjt-front/src/views/DetailView.vue
@@ -17,32 +17,54 @@
           </span>
         </span>
         <div class="overview">{{ detailViewData.movie?.overview }}</div>
-        <button @click="getsimilarMovies">비슷한 영화 보러 가기</button>
-        <button @click="getCrews">출연진 보기</button>
+        <div class="wrap-btn">
+          <button class="common-btn" @click="viewSimilarMovies">비슷한 영화 보러 가기</button>
+          <button class="common-btn" @click="viewCrews">출연진 보기</button>
+        </div>
       </div>
     </div>
-    <div v-for="cast in crews" :key="cast.id">{{cast.name}}
-      <img :src="`https://image.tmdb.org/t/p/w500/${cast.profile_path}`" alt="">
+    <div id="wrapCrew" class="toggle-crews">
+      <div class="wrap-crew">
+        <div class="crew-info">
+          <div class="hide-title">
+            <h3>DIRECTOR</h3>
+            <h4>[{{ director?.name }}]</h4>
+          </div>
+          <img
+            :src="`https://image.tmdb.org/t/p/w500/${director?.profile_path}`"
+            alt="profile"
+          />
+        </div>
+        <div class="crew-info" v-for="cast in crews" :key="cast.id">
+          <div class="hide-title">
+            <h3>CAST</h3>
+            <h4>[{{ cast.name }}]</h4>
+          </div>
+          <img
+            :src="`https://image.tmdb.org/t/p/w500/${cast?.profile_path}`"
+            alt="profile"
+          />
+        </div>
+      </div>
     </div>
     <div>
-      {{director?.name}}
-      <img :src="`https://image.tmdb.org/t/p/w500/${director.profile_path}`" alt="">
+      <button id="loadNewSimilarBtn" class="common-btn toggle-similar" @click="getSimilarMovies">새로운 영화 불러오기</button>
     </div>
-    <div class="wrap-similar">
+    <div id="wrapSimilar" class="wrap-similar toggle-similar">
       <div class="similar-movie" v-for="smovie in newmovies" :key="smovie.id">
+        <h3 class="hide-title">{{ smovie.title }}</h3>
         <img
           :src="`https://image.tmdb.org/t/p/w500/${smovie.poster_path}`"
           alt="smovie_poster"
         />
-        <h4 class="hide-title">{{ smovie.title }}</h4>
         <div>
-        <!-- <router-link
-          class="common-btn"
-          :to="{ name: 'DetailView', params: { id: smovie.id } }"
-          >DETAIL</router-link
-        > -->
-        </div>
+          <router-link
+            class="common-btn"
+            :to="{ name: 'DetailView', params: { id: smovie.id } }"
+            >DETAIL</router-link
+          >
       </div>
+    </div>
     </div>
     <div class="wrap-comment">
       <CommentList />
@@ -106,9 +128,9 @@ export default {
         });
     },
     submitComment() {
-      if (!localStorage.getItem('userName')){
-          alert('로그인이 필요합니다.')
-          this.$router.push({ name: "LoginView" });
+      if (!localStorage.getItem("userName")) {
+        alert("로그인이 필요합니다.");
+        this.$router.push({ name: "LoginView" });
       }
       axios({
         method: "post",
@@ -138,31 +160,46 @@ export default {
           console.log(err);
         });
     },
-    getsimilarMovies() {
+    getSimilarMovies() {
       axios({
         method: "get",
         url: `https://api.themoviedb.org/3/movie/${this.detailViewData.movie.id}/similar?api_key=616c881ba896937b008706b9a5e911d0&language=ko-KR&page=${this.similarPage}`,
       }).then((res) => {
         this.newmovies = res.data.results;
-        
-        this.similarPage += 1
+        this.similarPage += 1;
       });
     },
-    getCrews(){
+    viewSimilarMovies() {
+      const wrapSimilar = document.querySelector("#wrapSimilar");
+      const loadNewSimilarBtn = document.querySelector("#loadNewSimilarBtn");
+      if (this.newmovies.length === 0) {
+        this.getSimilarMovies();
+      }
+      loadNewSimilarBtn.classList.toggle("toggle-similar")
+      wrapSimilar.classList.toggle("toggle-similar");
+    },
+    getCrews() {
       axios({
-        method:'get',
-        url: `https://api.themoviedb.org/3/movie/${this.detailViewData.movie.id}/credits?api_key=616c881ba896937b008706b9a5e911d0&language=ko-KR`
+        method: "get",
+        url: `https://api.themoviedb.org/3/movie/${this.detailViewData.movie.id}/credits?api_key=616c881ba896937b008706b9a5e911d0&language=ko-KR`,
       })
-      .then((res)=>{
-        console.log(res.data)
-        this.crews = res.data.cast.slice(0,3)
-        this.director = res.data.crew.filter(crew => crew.job==='Director')[0]
-        console.log(this.director[0].name)
-      })
-      .catch((err)=>{
-        console.log(err)
-      });
-    }
+        .then((res) => {
+          this.crews = res.data.cast.slice(0, 3);
+          this.director = res.data.crew.filter(
+            (crew) => crew.job === "Director"
+          )[0];
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    viewCrews() {
+      const wrapCrew = document.querySelector("#wrapCrew");
+      if (this.crews.length === 0) {
+        this.getCrews();
+      }
+      wrapCrew.classList.toggle("toggle-crews");
+    },
   },
 };
 </script>
@@ -182,6 +219,9 @@ h1 {
 }
 .wrap-detail-para {
   padding: 0 10% 0 10%;
+}
+.wrap-btn button {
+  margin: 10px
 }
 .title {
   font-size: 2.4rem;
@@ -208,6 +248,7 @@ h1 {
   margin: 5% auto;
 }
 .wrap-similar {
+  background-color: rgba(216, 112, 147, 0.3);
   display: flex;
   flex-wrap: nowrap;
   overflow-x: scroll;
@@ -220,22 +261,41 @@ h1 {
   top: 50%;
   transform: translate(-50%, -50%);
 }
-.similar-movie {
+.similar-movie, .crew-info {
   cursor: pointer;
   position: relative;
   margin: 2%;
   width: 200px;
   flex: 0 0 auto;
 }
-
+.wrap-crew {
+  display: flex;
+  justify-content: space-evenly;
+  background-color: rgba(216, 112, 147, 0.3);
+  margin-bottom: 50px;
+}
+.crew-info img {
+  border-radius: 20%;
+  border: rgb(216, 192, 200) 4px solid;
+  width: 100%;
+}
 .similar-movie:hover > img {
+  opacity: 0.2;
+}
+
+.crew-info:hover > img {
   opacity: 0.2;
 }
 .similar-movie:hover .hide-title {
   display: block;
 }
-
+.crew-info:hover .hide-title {
+  display: block;
+}
 .similar-movie img {
   width: 100%;
+}
+.toggle-crews, .toggle-similar {
+  display: none;
 }
 </style>
