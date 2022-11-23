e3c7e0c2c14fda41071715bd03052a5e011fa34f
RYU SEUNG KYU <shark087210@gmail.com>
2022-11-23 16:37:00 +0900

    algorithm completed

--- final-pjt-front/src/components/RecommendList.vue
+++ final-pjt-front/src/components/RecommendList.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    {{ likeList }}
     <div class="movie-filter">
+      <span class="common-btn" @click="bringLike"
+        >좋아요 기반 다시 추천받기</span
+      >
       <div
         :class="{ 'common-btn': !isClassic, 'common-clicked-btn': isClassic }"
         @click="toggleClassic"
@@ -22,7 +24,7 @@
     </div>
     <div class="wrap-recommand-list">
       <RecommendListItem
-        v-for="movie in movies"
+        v-for="movie in algoList"
         :key="movie.id"
         :movie="movie"
         :isClassic="isClassic"
@@ -35,6 +37,7 @@
 <script>
 import RecommendListItem from "./RecommendListItem.vue";
 import axios from "axios";
+import _ from "lodash"
 export default {
   name: "RecommendList",
   components: {
@@ -42,35 +45,37 @@ export default {
   },
   data() {
     return {
-      username : localStorage.getItem('userName'),
+      username: localStorage.getItem("userName"),
       isClassic: false,
       isIndependent: false,
-      movie_genres : {   
-      '액션' : 5,
-      '모험' : 5,
-      '애니메이션' : 5,
-      '코미디' : 5,
-      '범죄' : 5,
-      '다큐멘터리' : 5,
-      '드라마' : 5,
-      '가족' : 5,
-      '판타지' : 5,
-      '역사' : 5,
-      '공포' : 5,
-      '음악' : 5,
-      '미스터리' : 5,
-      '로맨스' : 5,
-      'SF' : 5,
-      'TV 영화' : 5,
-      '스릴러' : 5,
-      '전쟁' : 5,
-      '서부' : 5,
+      like_genres: [],
+      movie_genres: {
+        28: 5, // 액션
+        12: 5, // 모험
+        16: 5, // 애니메이션
+        35: 5, // 코미디
+        80: 5, // 범죄
+        99: 5, // 다큐멘터리
+        18: 5, // 드라마
+        10751: 5, // 가족
+        14: 5, // 판타지
+        36: 5, // 역사
+        27: 5, // 공포
+        10402: 5, // 음악
+        9648: 5, // 미스터리
+        10749: 5, // 로맨스
+        878: 5, // SF
+        10770: 5, // TV 영화
+        53: 5, // 스릴러
+        10752: 5, // 전쟁
+        37: 5, // 서부
       },
-    likeList: [],
+      likeList: [],
+      algoList: [],
     };
   },
   created() {
-    this.bringLike()
+    this.bringLike();
   },
   computed: {
     movies() {
@@ -88,18 +93,48 @@ export default {
       this.isIndependent = !this.isIndependent;
     },
     bringLike() {
+      this.algoList = [];
       axios({
         method: "get",
         url: `http://127.0.0.1:8000/accounts/liked/${this.username}/`,
       })
         .then((res) => {
-          this.likeList = res.data.liked_movie
-          console.log(this.movie_genres)
-        
+          this.likeList = res.data.liked_movie;
+          // liketList에 해당하는 각각의 장르들을 저장(like_genres)
+          for (const movie of this.movies) {
+            if (this.likeList.includes(movie.id)) {
+              this.like_genres.push(...movie.genre_ids);
+            }
+          }
+          // 여기서 포문을 돌려 전체 장르(movie_genres)에서 좋아하는 장르(likeList.movie.genre_ids)에 점수를 주고싶음.
+          for (const like_genre of this.like_genres) {
+            this.movie_genres[like_genre] += 5;
+          }
+          // 그 점수를 기반으로 새로운 리스트 정렬한 기준 완성.
+          let movieScore = -1;
+          for (const movie of this.movies) {
+            movieScore = 0;
+            for (const genreId of movie.genre_ids) {
+              movieScore += this.movie_genres[genreId];
+            }
+            movieScore = parseInt(movieScore / movie.genre_ids.length);
+            this.algoList.push({
+              movieScore: movieScore,
+              id: movie.id,
+              title: movie.title,
+              poster_path: movie.poster_path,
+              release_date: movie.release_date,
+              vote_average: movie.vote_average,
+              vote_count: movie.vote_count,
+            });
+          }
+          this.algoList.sort((a, b) => b["movieScore"] - a["movieScore"])
+          this.algoList = _.sampleSize(this.algoList.slice(0, 300), 100)
+          // 그 점수를 기반으로 새로운 movies를 만들어 정렬후 리스트로 보내주면 렌더링 끝.
         })
         .catch((err) => {
-          console.log(err)
-        })
+          console.log(err);
+        });
     },
   },
 };
